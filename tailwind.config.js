b46547390599afa6bef215079917d3dfcf77ec7c
박성찬 <goslow0628@gmail.com>
2025-06-27 18:24:19 +0900

    test 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 // tailwind.config.js
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,md,mdx}", // ← 여긴 있음
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,md,mdx}", // ← 여긴 있음
+    "./src/components/**/*.{js,ts,jsx,tsx}",
     "./posts/**/*.{md,mdx}", // ← 마크다운도 OK
   ],
   theme: {
