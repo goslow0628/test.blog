7d71de266fffeea9a2acf85b24a63df5829851ec
박성찬 <goslow0628@gmail.com>
2025-06-27 19:05:27 +0900

    test 11

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+// src/app/page.tsx
 import Header from "@/components/Header";
 import Profile from "@/components/Profile";
 import PostList from "@/components/PostList";
@@ -10,13 +11,30 @@ export default function Home() {
       <Header />
 
       {/* 중앙 콘텐츠 + 우측 태그 사이드바 */}
-      <div style={{ display: "flex", maxWidth: "960px", margin: "0 auto" }}>
-        <main style={{ flex: 1 }}>
+      <div
+        style={{
+          maxWidth: "1024px",
+          margin: "0 auto",
+          padding: "2rem",
+          display: "flex",
+          gap: "2rem",
+        }}
+      >
+        {/* 본문 */}
+        <div
+          style={{
+            flex: 1,
+            display: "flex",
+            flexDirection: "column",
+            gap: "2rem",
+          }}
+        >
           <Profile />
           <PostList />
-        </main>
+        </div>
 
-        <aside style={{ width: "200px" }}>
+        {/* 사이드바 */}
+        <aside style={{ width: "240px" }}>
           <TagSidebar />
         </aside>
       </div>
